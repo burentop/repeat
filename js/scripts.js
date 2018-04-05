ef3537d2328ed1fab2b7b540507d2d2bc7f46936
Brent Perry (DAL - ACCF) <burentoperii@gmail.com>
2018-04-05 14:20:47 -0500

    complete form submission/display logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $(".submit form").submit(function(event) {
-    
+    var phraseInput = $("#phrase").val().toUpperCase();
+    $("#response-line").text(phraseInput);
+    $(".response").toggle();
+
+    event.preventDefault();
   });
 });
